3b6c84718102f26f41d617b4270cdb4e3c4f455a
Kashif <km092009@gmail.com>
2021-12-15 15:58:50 -0500

    custom hook function set up

--- src/components/hooks/useApplicationData.js
+++ src/components/hooks/useApplicationData.js
@@ -1,49 +1,19 @@
 import { useReducer, useEffect } from "react";
 import axios from "axios";
-import reducer, {
-  SET_DAY,
-  SET_APPLICATION_DATA,
-  SET_INTERVIEW,
-} from "components/reducers/application";
+
 
 // Functions used to update state upon booking and cancelling an Interview
 export default function useApplicationData() {
   function bookInterview(id, interview) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview },
-    };
-
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-
-    dispatch({
-      type: SET_INTERVIEW,
-      id: id,
-      interview: interview,
-    });
-    return axios.put(`api/appointments/${id}`, { interview });
+    
+  
   }
 
   function cancelInterview(id) {
-    return axios.delete(`api/appointments/${id}`).then(() => {
-      const delAppointment = { ...state.appointments[id], interview: null };
-      const appointments = { ...state.appointments, [id]: delAppointment };
-
-      dispatch({ type: SET_INTERVIEW, id: id, interview: null });
-    });
+   
   }
 
-  const [state, dispatch] = useReducer(reducer, {
-    day: "Monday",
-    days: [],
-    appointments: {},
-    interviewers: {},
-  });
 
-  const setDay = (day) => dispatch({ type: SET_DAY, day: day });
 
   useEffect(() => {
     Promise.all([
@@ -51,12 +21,7 @@ export default function useApplicationData() {
       axios.get("api/appointments"),
       axios.get("api/interviewers"),
     ]).then((arrPromiseResponse) => {
-      dispatch({
-        type: SET_APPLICATION_DATA,
-        days: arrPromiseResponse[0].data,
-        appointments: arrPromiseResponse[1].data,
-        interviewers: arrPromiseResponse[2].data,
-      });
+     
     });
   }, []);
   return {
