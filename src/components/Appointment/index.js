22a19117a91238cb19226127cc9782d2d4e89cb4
Kashif <km092009@gmail.com>
2021-12-14 20:31:04 -0500

    appointments module updated

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,17 +1,67 @@
-import React from "react";
-import "components/Appointment/styles.scss";
-import Header from "components/Appointment/Header.js";
-import Empty from "components/Appointment/Empty.js";
-import Confirm from "components/Appointment/Confirm.js";
-import Error from "components/Appointment/Error.js";
-import Form from "components/Appointment/Form.js";
-import Show from "components/Appointment/Show.js";
-import Status from "components/Appointment/Status.js";
-//import InterviewerListItem from "components/InterviewerListItem.js";
+import React from 'react';
+import Header from 'components/Appointment/Header';
+import Show from 'components/Appointment/Show';
+import Empty from 'components/Appointment/Empty';
+import Form from 'components/Appointment/Form';
+import useVisualMode from 'components/hooks/useVisualMode';
+import 'components/Appointment/styles.scss';
+import Status from './Status';
+import Confirm from 'components/Appointment/Confirm';
+import Error from 'components/Appointment/Error';
 
-export default function Appointment(props){
-  
-  return(
-    <article className="appointment">{props.time ? 'Appointment at ' + props.time:'No Appointments'}</article>
-  )
+// Transition View States
+const EMPTY = 'EMPTY';
+const SHOW = 'SHOW';
+const CREATE = 'CREATE';
+const SAVING = 'SAVING';
+const DELETING = 'DELETING';
+const EDITING = 'EDITING';
+const CONFIRMING = 'CONFIRMING';
+const ERROR_SAVE = 'ERROR_SAVE';
+const ERROR_DELETE = 'ERROR_DELETE';
+
+export default function Appointment(props) {
+  // Save an appointment
+  function save(name, interviewer) {
+    transition(SAVING);
+    const interview = {
+      student: name,
+      interviewer
+    };
+    props
+      .bookInterview(props.id, interview)
+      .then(response => {
+        transition(SHOW);
+      })
+      .catch(error => transition(ERROR_SAVE, true));
+  }
+
+  // Delete an appointment
+  function deleting() {
+    transition(DELETING, true);
+    props
+      .cancelInterview(props.id)
+      .then(() => {
+        transition(EMPTY);
+      })
+      .catch(error => transition(ERROR_DELETE, true));
+  }
+
+  function edit() {
+    transition(EDITING);
+  }
+
+  function confirmation() {
+    transition(CONFIRMING);
+  }
+
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
+
+  return (
+    <article data-testid="appointment" className="appointment">
+      
+    </article>
+  );
 }
