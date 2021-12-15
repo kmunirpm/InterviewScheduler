bfd03c035c787bd3886150f876d32a44d18f6e10
Kashif <km092009@gmail.com>
2021-12-14 20:49:24 -0500

    helper functions completed

--- src/components/helpers/selectors.js
+++ src/components/helpers/selectors.js
@@ -1,9 +1,9 @@
-export function getAppointmentsForDay(state, day) {
-  //... returns an array of appointments for that day
+// gets all the all the appointments for the selected day "dayName" - returns array
+export function getAppointmentsForDay(state, dayName) {
   let retarr = [];
   if (state.days.length < 1) return [];
   const dayAppointmentIDs = state.days.filter(
-    (currday) => currday.name === day
+    (currday) => currday.name === dayName
   );
   if (dayAppointmentIDs.length < 1) return [];
   Object.keys(state.appointments).forEach((element) => {
@@ -13,6 +13,7 @@ export function getAppointmentsForDay(state, day) {
   return retarr;
 }
 
+// gets info about the selected interview "interview" - returns an object with student & interviewer names
 export function getInterview(state, interview) {
   if (!interview) return null;
   const ivObj = {
@@ -23,9 +24,10 @@ export function getInterview(state, interview) {
   return ivObj;
 }
 
+//gets all the interviews for the selected day "dayName" - returns array
 export function getInterviewersForDay(state, dayName) {
-  const validDayNames = state.days.map(dayObj => dayObj.name);
-  if (!dayName || !validDayNames.includes(dayName)) return [];
+  const valDayNames = state.days.map(dayObj => dayObj.name);
+  if (!dayName || !valDayNames.includes(dayName)) return [];
 
   const todayObj = state.days.filter(dayObj => dayObj.name === dayName)[0];
   const interviewersObj = todayObj.interviewers.map(
